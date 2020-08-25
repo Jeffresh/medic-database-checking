3471086dd3c64f6aa7587c4e5b04e9f6b4f24175
Jeff <jeff.uca@gmail.com>
2020-08-25 10:52:28 +0200

    :sparkles: add code to generate rows indices

--- src/data/medic_database_checking.py
+++ src/data/medic_database_checking.py
@@ -1,11 +1,24 @@
+import string
+
+letter_UP = string.ascii_uppercase
+digits = string.digits
+rows = {letter: row for (row, letter) in enumerate(letter_UP[:11])}
+rows.update({'X': 11})
+
+
 def get_data(file_path):
     with open(file_path) as medic_database:
         data = medic_database.readlines()
     return data
 
 
+def check_entries():
+    pass
+
+
 if __name__ == '__main__':
     path = '../data/doctors_list'
     data_entries = get_data(path)
-    for entry in data_entries:
-        print(entry)
+    # for entry in data_entries:
+    #     print(entry)
+
